8a8af246bc4de9721cf70e8da803e894ed762780
enginoir <myusuf141012@gmail.com>
2023-09-25 20:02:38 +0700

    ADD : Controller.js in DIR controller

---
+++ backend/controller/Controller.js
@@ -0,0 +1,44 @@
+//update isi controller nanti
+const db = require("../sequelize/models");
+
+class Controller {
+    constructor(modelname) {
+        this.modelname = modelname;
+        this.db = db[modelname];
+    }
+    getAll(req, res) {
+        this.db
+        .findAll()
+        .then((result) => res.send(result))
+        .catch((err) => res.status(500).send(err?.message));
+    }
+    getById(req, res) {
+        const { id } = req.params;
+        this.db
+        .findByPk(id)
+        .then((result) => res.send(result))
+        .catch((err) => res.status(500).send(err?.message));
+    }
+    create(req, res) {
+        this.db
+        .create({ ...req.body })
+        .then((result) => res.send(result))
+        .catch((err) => res.status(500).send(err?.message));
+    }
+    update(req, res, next) {
+        const { id } = req.params;
+        this.db
+        .update({ ...req.body }, { where: { id } })
+        .then(() => next())
+        .catch((err) => res.status(500).send(err?.message));
+    }
+    delete(req, res) {
+        const { id } = req.params;
+        this.db
+        .destroy({ where: { id } })
+        .then(() => res.send({ message: `${this.modelname} deleted` }))
+        .catch((err) => res.status(500).send(err?.message));
+    }
+}
+
+module.exports = Controller;
