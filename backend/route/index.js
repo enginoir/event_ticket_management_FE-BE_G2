01ae1535377f32e21d950334877d442489b48730
enginoir <myusuf141012@gmail.com>
2023-10-08 10:21:17 +0700

    MOD: Route Index, Transaction Validator

--- backend/route/index.js
+++ backend/route/index.js
@@ -2,10 +2,12 @@ const userRouter = require("./user_router");
 const locationRouter = require("./location_router.js");
 const eventRouter = require("./eventsRouter");
 const uploadRouter = require("./uploadRouter");
+const promotionRouter = require("./promotionRouter");
 
 module.exports = {
+    promotionRouter: promotionRouter,
     userRouter: userRouter,
     locationRouter: locationRouter,
     eventRouter: eventRouter,
-  uploadRouter: uploadRouter,
+    uploadRouter: uploadRouter,
 };
